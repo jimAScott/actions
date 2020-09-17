65814c1935f94c6f6ff7510dbd2af6d776faf7cd
JamesScott <jim_a_scott@hotmail.com>
2020-09-17 14:44:31 +0100

    update

--- site/gridsome.server.js
+++ site/gridsome.server.js
@@ -5,12 +5,14 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-module.exports = function (api) {
+module.exports = function(api) {
   api.loadSource(({ addCollection }) => {
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
-  })
+
+    console.log(`The environment you are using is ${process.env.TEST_VAR}`);
+  });
 
   api.createPages(({ createPage }) => {
     // Use the Pages API here: https://gridsome.org/docs/pages-api/
-  })
-}
+  });
+};
